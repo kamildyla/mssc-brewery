cfcc7efc817b1a7c07baf1d2582d1ebb09831901
kamildyla <kamil.dyla09@gmail.com>
2020-11-19 21:26:29 +0100

    added customerDto

---
+++ src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbrewery.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CustomerDto {
+
+    private String name;
+}
