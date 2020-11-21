f0c80b1a5ca1f1ce60a4c5bf94b4d3ab286ef7c9
kamildyla <kamil.dyla09@gmail.com>
2020-11-21 22:44:21 +0100

    add POST PUT DELETE customer

--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -1,10 +1,12 @@
 package guru.springframework.msscbrewery.services;
 
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService{
 
@@ -14,5 +16,22 @@ public class CustomerServiceImpl implements CustomerService{
                 .id(UUID.randomUUID())
                 .customerName("Joe Buck")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        // todo impl - would add a real impl to update customer
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting a customer...");
     }
 }
